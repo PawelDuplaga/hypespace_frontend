be5b6fdcceae9279800030edecafb346f0d27ce7
PawelDuplaga <pawelduplagait@gmail.com>
2023-06-25 20:12:07 +0200

    carousel improved

--- components/Carousel/StaticCarousel/StaticCarousel.tsx
+++ components/Carousel/StaticCarousel/StaticCarousel.tsx
@@ -5,31 +5,35 @@ import ProductViewTemplateB from '@/components/ProductViewComponents/ProductView
 
 export default function StaticCarousel () {
 
-    const AnimationFazes : string[] = ["default", "animate0", "animate1", "animate2"]
+    const AnimationFazes : string[] = ["default", "animate0", "animate1"]
 
     const [isAnimating, setIsAnimating] = useState(true);
     const [animationFazeIndex, setAnimationFazeIndex] = useState(0)
     const [buttonsVisibility, setButtonsVisibility] = useState({left : false, right : true})
 
-
-    const handleClickRightArrow = () => {
-
-      setAnimationFazeIndex(prev => prev +1)
-      console.log(AnimationFazes[animationFazeIndex])
-      // czemu nie pokazuje nowego stanu
+    const handleClickRightButton = () => {
+        if(animationFazeIndex < AnimationFazes.length - 1) {
+            setAnimationFazeIndex(prev => prev +1)
+        }
     };
-  
+
+    const handleClickLeftButton = () => {
+        if(animationFazeIndex > 0){
+            setAnimationFazeIndex(prev => prev - 1)
+        }
+    }
+
     const onAnimationEnd = () => {
         setButtonsVisibility(prevState => ({
-            ...prevState,
-            left: true
+            right : animationFazeIndex !== AnimationFazes.length - 1,
+            left: animationFazeIndex !== 0
           }));
       console.log("end")
       console.log(AnimationFazes[animationFazeIndex])
     };
     
 
-    const currProducts : number[] = [1,2,3,4,5,6,7,8,9,10]
+    const currProducts : number[] = [1,2,3,4,5,6,7,8,9,10,11,12]
 
 
     function mapRecomendedSection () {
@@ -44,14 +48,15 @@ export default function StaticCarousel () {
     }
 
 
+
     return (
         <div className='carousel-container'>
             {buttonsVisibility.right && 
-            <div className="button-right"onClick={handleClickRightArrow}>
+            <div className="button-right"onClick={handleClickRightButton}>
                 <img className='arrow-icon' src="./icons/maki_arrow.svg" />
             </div>}
             {buttonsVisibility.left &&
-            <div className="button-left" onClick={handleClickRightArrow}>
+            <div className="button-left" onClick={handleClickLeftButton}>
                 <img className='arrow-icon' src="./icons/maki_arrow.svg" />
             </div>}          
             <div 
