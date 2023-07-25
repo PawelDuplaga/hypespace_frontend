7f002f11e5bcef3f065410b00490f816f8e2a6f3
PawelDuplaga <pawelduplagait@gmail.com>
2023-07-26 00:43:14 +0200

    test commit

--- app/Products/[index]/page.tsx
+++ app/Products/[index]/page.tsx
@@ -6,39 +6,58 @@ import ProductViewTemplateB from "@/components/ProductViewComponents/ProductView
 import { useState, useEffect } from "react";
 import styles from "./page.module.scss";
 import { fetchRandomProducts } from "@/RequestMock/dataMock";
-import PageIndexer from "@/components/Page_GridProducts/PageIndexer/PageIndexer";
+import Pagination from "@mui/material/Pagination";
+import PaginationItem from "@mui/material/PaginationItem"; 
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import Link from "next/link";
 
 const NUMBER_OF_PRODUCTS_PER_PAGE : number = 16;
 
-function ProductsGridPage ({params} : {params: {index : number}}) {
+function ProductsGridPage ({params} : {params: {index : string}}) {
 
 
     const [products, setProducts] = useState<PricedProduct[]>();
-    const [pageNumber, setPageNumber] = useState<number>(1);
 
     useEffect(() => {
         fetchRandomProducts(NUMBER_OF_PRODUCTS_PER_PAGE).then((products) => {
             setProducts(products);
-            console.log(products);
+            console.log(params.index)
+            //console.log(products);
         });
-    }, [pageNumber]);
+    }, []);
 
+    
     // narazie mapuje 1 produkt do wszystkich okien
+
+    const currPage = parseInt
 
     return (
         <div className={styles.mainContainer}>
-            <div className={styles["grid-container"]}>
+            <div className={styles.titleContainer}>
+                <div className={styles.gridPageTitle}>Wszystkie Produkty</div>
+            </div>
+            <div className={styles.gridContainer}>
                 {products?.map((item, index) => (
-                    <div className={styles["grid-item"]} key={index}>
+                    <div className={styles.gridItem} key={index}>
                         <ProductViewTemplateB product={item} />
                     </div>
                 ))}
             </div>
-            <PageIndexer 
-                numberOfPages={10}
-                currentPage={pageNumber}
-                setPageCallback={setPageNumber}
-                mainRoute='products'
+            <Pagination
+                page={parseInt(params.index)}
+                defaultPage={1}
+                siblingCount={2}
+                boundaryCount={2}
+                count={20}
+                renderItem={(item) => (
+                    <PaginationItem
+                    component={Link}
+                    href={`/Products/${item.page}`}
+                    slots={{ previous: ArrowBackIcon, next: ArrowForwardIcon }}
+                    {...item}
+                    />
+                )}
             />
         </div>
     );
